3bf5b55f9bfa80237f479cf6e528b5ef6c3d4ef4
edgar397 <85273127+edgar397@users.noreply.github.com>
2021-06-02 22:57:17 -0700

    Initial commit

---
+++ java.js/scripts.js
@@ -0,0 +1,14 @@
+const nombre = document.querySelector('select');
+const dar = document.getElementById('mensage');
+
+
+nombre.addEventListener('copy', CarUser())
+
+    const CarUser = ()=>{
+ let fuente = nombre.value 
+ getSelection('')
+    }
+
+
+
+
